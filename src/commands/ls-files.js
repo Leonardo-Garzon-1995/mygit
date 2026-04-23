915cac132879866d8981fe616a61856515c01658
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2026-04-23 08:11:48 +0200

    add-feature: ls-files command

---
+++ src/commands/ls-files.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+
+const readIndex = require('../helpers/readIndex')
+const { ensureRepo } = require('../core/repository')
+
+
+
+function lsFiles() {
+    ensureRepo()
+
+    const index = readIndex()
+    if (!index.entries || Object.keys(index.entries).length === 0) {
+        return
+    }
+
+    const sorted = Object.keys(index.entries).sort()
+
+    for (const filePath of sorted) {
+        console.log(filePath)
+    }
+}
+
+module.exports = lsFiles
