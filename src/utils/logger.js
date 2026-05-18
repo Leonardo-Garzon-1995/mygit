2ddd93fd4598aeac5bcea238566b32cb4a6217b2
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2026-05-18 07:00:44 +0200

    chore: add a logger for debuging purposses

---
+++ src/utils/logger.js
@@ -0,0 +1,63 @@
+const fs = require('fs')
+const path = require('path')
+
+const LOG_DIR = path.join(__dirname, '..', '..', 'logs')
+const LOG_FILE = path.join(LOG_DIR, "mygit.log")
+
+function ensureLogFile() {
+    if (!fs.existsSync(LOG_DIR)) {
+        fs.mkdirSync(LOG_DIR, {recursive: true})
+    }
+
+    if (!fs.existsSync(LOG_FILE)) {
+        fs.writeFileSync(LOG_FILE, '')
+    }
+}
+
+function getTimeStamp() {
+    return new Date().toISOString()
+}
+
+function write(mode, message) {
+    ensureLogFile()
+    
+    const line = `[${getTimeStamp()}] [${mode}] ${message}\n`
+
+    fs.appendFileSync(LOG_FILE, line)
+}
+
+function error(message) {
+    if (!message) {
+        return
+    }
+    write("ERROR", message)
+}
+
+function debug(message) {
+    if (!message) {
+        return
+    }
+    write("DEBUG", message)
+}
+
+function info(message) {
+    if (!message) {
+        return
+    }
+    write("INFO", message)
+}
+
+function warn(message) {
+    if (!message) {
+        return
+    }
+    write("WARN", message)
+}
+
+module.exports = {
+    error,
+    debug,
+    info,
+    warn
+}
+
