ef49b472d9a2339660aeabbe60f4391da3235516
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2026-05-25 03:44:08 +0200

    refactor: add 'paths.js' for repo related paths

---
+++ src/core/repository/paths.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+function objectPath(repo, hash) {
+    return path.join(repo.paths.objects, hash.slice(0, 2), hash.slice(2))
+}
+
+function refPath(repo, ref) {
+    return repo.resolveMygitPath(ref)
+}
+
+function branchPath(repo, branchName) {
+    return repo.resolveMygitPath('refs', 'heads', branchName)
+}
+
+function tagPath(repo, tagName) {
+    return repo.resolveMygitPath('refs', 'tags', tagName)
+}
+
+module.exports = {
+    objectPath,
+    refPath,
+    branchPath,
+    tagPath
+}
