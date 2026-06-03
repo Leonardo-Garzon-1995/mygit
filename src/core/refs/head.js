7760961589f308788247984f2d15029925e5a14e
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2026-06-03 07:25:28 +0200

    refactor(refs): add head.js module. It contains all the reference logic for the HEAD file

---
+++ src/core/refs/head.js
@@ -0,0 +1,72 @@
+const fs = require('../../utils/filesystem')
+
+const { refPath } = require('../repository/paths')
+const { InvalidReferenceError } = require('../../errors')
+
+
+function readHEAD(repo) {
+    const headPath = repo.paths.head
+
+    if (!fs.exists(headPath)) {
+        throw new InvalidReferenceError('HEAD does not exists')
+    }
+
+    return fs.readFile(headPath).trim()
+}
+
+function isSymbolicHEAD(repo) {
+    return readHEAD(repo).startsWith('ref: ')
+}
+
+function getHEADref(repo) {
+    const head = readHEAD(repo)
+
+    if (!isSymbolicHEAD(repo)) return null
+
+    return head.replace(/^ref:\s*/, '')
+}
+
+// Get detached heas
+function getHEADHash(repo) {
+    const head = readHEAD(repo)
+
+    if (isSymbolicHEAD) return null
+
+    return head
+}
+
+function setHEADRef(repo, ref) {
+    fs.writeFile(repo.paths.head, `ref: ${ref}\n`)
+}
+
+function detachHEAD(repo, hash) {
+    fs.writeFile(repo.paths.head, `${hash}\n`)
+}
+
+function resolveHEAD(repo) {
+    const head = readHEAD(repo)
+
+    if (!isSymbolicHEAD(repo)) {
+        return head
+    }
+
+    const ref = getHEADref(repo)
+
+    const filePath = refPath(repo, ref)
+
+    if (!fs.exists(filePath)) {
+        return null
+    }
+
+    return fs.readFile(filePath).trim()
+}
+
+module.exports = {
+    readHEAD,
+    isSymbolicHEAD,
+    getHEADref,
+    getHEADHash,
+    setHEADRef,
+    detachHEAD,
+    resolveHEAD
+}
