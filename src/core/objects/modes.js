ae26fc519b100de6f7751459d3e7148c2b2676a4
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2026-06-04 04:41:23 +0200

    refactor(objects/modes): add module containing function to get the mode of a file

---
+++ src/core/objects/modes.js
@@ -0,0 +1,33 @@
+const fs = require('fs') 
+
+// Maybe move later into /src/utils/filesystem.js
+
+/**
+ * Returns the mode of a file given its path.
+ * The mode is a string that represents the file type and its executable status.
+ * If the file does not exist, an empty string is returned.
+ * @param {string} filePath - the path of the file
+ * @returns {string} the mode of the file
+ */
+function getFileMode(filePath) {
+
+    let stats;
+    try {
+        stats = fs.lstatSync(filePath)
+    } catch (err) {
+        return ""
+    }
+
+    if (stats.isDirectory()) {
+        return '40000'
+    }
+
+    if (stats.isSymbolicLink()) {
+        return '120000'
+    }
+
+    const isExecutable = (stats.mode & 0o111) !== 0
+    return isExecutable ? '100755' : '100644'
+}
+
+module.exports = getFileMode
