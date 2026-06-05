b83f6e7428f90cba33bb5bb55a3ba8483da55b44
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2026-06-05 12:36:58 +0200

    refactor(refs): centralize all refs operations in one module
    this is the core logic to be used in heads and tags

---
+++ src/core/refs/references.js
@@ -0,0 +1,87 @@
+const path = require('../../utils/paths')
+const fs = require('../../utils/filesystem')
+const { ValidationError, InvalidReferenceError } = require('../../errors')
+const { isValidHash, isValidRef, isValidBranchName } = require('../../utils/validation')
+
+function validateReferenceName(name) {
+    if (!isValidBranchName(name)) {
+        throw new ValidationError(`Invalid reference: ${name}`)
+    }
+}
+
+
+// Core operations 
+
+function createReference(filePath, hash='') {
+    if (hash !== '' && !isValidHash(hash)) {
+        throw new ValidationError(`Invalid hash: ${hash}`)
+    }
+
+    fs.ensureDir(path.dirname(filePath))
+
+    fs.writeFile(filePath, hash ? `${hash}\n` : '')
+}
+
+function readReference(filePath) {
+    if (!fs.exists(filePath)) {
+        throw new InvalidReferenceError(`Reference dos not exist: ${filePath}`)
+    }
+
+    return fs.readFile(filePath).trim()
+}
+
+function updateReference(filePath, hash) {
+    if (!fs.exists(filePath)) {
+        throw new InvalidReferenceError(`Reference does not exist: ${filePath}`)
+    }
+    if (!isValidHash(hash)) {
+        throw new ValidationError(`Invalid hash: ${hash}`)
+    }
+
+    fs.writeFile(filePath, `${hash}\n`)
+}
+
+function deleteReference(filePath) {
+    if (!fs.exists(filePath)) {
+        throw new InvalidReferenceError(`Reference does not exist: ${filePath}`)
+    }
+
+    fs.removeFile(filePath)
+}
+
+// Queries 
+
+function referenceExists(filePath) {
+    return fs.exists(filePath)
+}
+
+/**
+ * Build full reference name
+ * 
+ * Example: refs/heads/main or refs/tags/v1.0.0
+ * 
+ * @param {string} namespace 
+ * @param {string} name 
+ * @returns {string}
+ */
+function buildReference(namespace, name) {
+    validateReferenceName(name)
+
+    const ref = `refs/${namespace}/${name}`
+
+    if (!isValidRef(ref)) {
+        throw new ValidationError(`Invalid reference: ${ref}`)
+    }
+
+    return ref
+}
+
+module.exports = {
+    createReference,
+    readReference,
+    updateReference,
+    deleteReference,
+    referenceExists,
+    buildReference,
+    validateReferenceName
+}
